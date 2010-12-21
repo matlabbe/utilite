2042ba881caac1bca9a83db9cb9d9a75b9aff8fd
matlabbe <matlabbe@6ff0932f-a43a-70d7-ab7a-1ab2c10f265d>
2010-12-21 21:32:52 +0000

    Version 0.2.4 Hided inherited methods Join(), Detach(), Kill() in UThreadNode. UEvent is not abstract anymore Added Producer-Consumer example

--- trunk/examples/simpleThreadExample.cpp
+++ trunk/examples/simpleThreadExample.cpp
@@ -1,11 +1,11 @@
-#include "utilite/UThreadNode.h"
+#include <utilite/UThreadNode.h>
 #include <stdio.h>
 
 /**
- * This is a simple example on how to use the StateThread class.
+ * This is a simple example on how to use the UThreadNode class.
  * The output of this program is :
  * @code
- * 		Example with the StateThread class
+ * 		Example with the UThreadNode class
  * 		This is called once before entering the main thread loop.
  * 		This is the loop 1...
  * 		This is the loop 2...
@@ -19,49 +19,43 @@
 class SimpleThread : public UThreadNode
 {
 public:
-	SimpleThread() : _loopCount(0) {}
-	~SimpleThread()
-	{
+	SimpleThread() : loopCount_(0) {}
+	~SimpleThread() {
 		printf("SimpleThread destructor\n");
 		this->kill();
 	}
 
 protected:
-	virtual void startInit()
-	{
+	virtual void startInit() {
 		printf("This is called once before entering the main thread loop.\n");
-		_loopCount = 1;
+		loopCount_ = 1;
 	}
 
-	virtual void mainLoop()
-	{
-		if(_loopCount < 3)
-		{
-			printf("This is the loop %d...\n", _loopCount++);
+	virtual void mainLoop() {
+		if(loopCount_ < 3) {
+			printf("This is the loop %d...\n", loopCount_++);
 			uSleep(10);
 		}
-		else
-		{
+		else {
 			printf("This thread will now wait on a semaphore...\n");
-			_aSemaphore.acquire();
+			semaphore_.acquire();
 			printf("Thread woke up!\n");
 		}
 	}
 
-	virtual void killCleanup()
-	{
+	virtual void killCleanup() {
 		printf("Releasing the semaphore...\n");
-		_aSemaphore.acquire();
+		semaphore_.release();
 	}
 private:
-	int _loopCount;
-	USemaphore _aSemaphore;
+	int loopCount_;
+	USemaphore semaphore_;
 };
 
 int main(int argc, char * argv[])
 {
 	SimpleThread t;
-	printf("Example with the StateThread class\n\n");
+	printf("Example with the UThreadNode class\n\n");
 	t.start();
 	uSleep(100);
 	t.kill();
